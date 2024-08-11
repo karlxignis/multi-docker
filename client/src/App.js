58552cf309662b3bca79f9e2e596ecd758bca885
Your Name <you@example.com>
2024-08-11 09:20:43 +0200

    Multi-Container Travis CI Version 3

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Ignis. Forever. And beyond
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
